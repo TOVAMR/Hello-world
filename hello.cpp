a55d08f22991690aa92afd1496bc49c42e0bdbf1
Tova M. Rosenberg <58287631+TOVAMR@users.noreply.github.com>
2020-06-15 12:31:50 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include "iostream"
 #include "hello.h"
-using namespace std;
+//using namespace std;
 void print_hello()
 {
-cout<<"hello world from my git!"<<endl;
+std::cout<<"hello world from my git!"<<std::endl;
 }
